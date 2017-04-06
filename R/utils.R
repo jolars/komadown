0fa69a0ca98bc05d0e1e275a62f3d7c9bb112464
Johan Larsson <johanlarsson@outlook.com>
2017-04-06 21:04:30 +0200

    Add utils.

---
+++ R/utils.R
@@ -0,0 +1,33 @@
+find_file <- function(template, file) {
+  template <- system.file("rmarkdown", "templates", template, file,
+                          package = "rmdtemplates")
+  if (template == "") {
+    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+  }
+
+  template
+}
+
+find_resource <- function(template, file) {
+  find_file(template, file.path("resources", file))
+}
+
+inherit_pdf_document <- function(...) {
+  fmt <- bookdown::pdf_document2(...)
+  fmt$inherits <- "pdf_document"
+  fmt
+}
+
+pdf_document_format <- function(..., format, template, csl) {
+
+  # base format
+  fmt <- inherit_pdf_document(..., template = find_resource(format, template))
+
+  # add csl to pandoc_args
+  fmt$pandoc$args <- c(fmt$pandoc$args,
+                       "--csl",
+                       rmarkdown::pandoc_path_arg(find_resource(format, csl)))
+
+  # return format
+  fmt
+}
