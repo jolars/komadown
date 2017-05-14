f997443c47e7f6513dc5dd254bfaeee660e05bc7
Johan Larsson <johanlarsson@outlook.com>
2017-05-14 15:48:31 +0200

    Explicitly provide automark.

--- R/lab_report.R
+++ R/lab_report.R
@@ -3,6 +3,8 @@
 #' A lab report template.
 #'
 #' @inheritParams bookdown::pdf_document2
+#' @param automark Uses `scrlayer-scrpage` to include the subsection and section
+#'   in the header (upper left).
 #' @param ... Arguments to \code{bookdown::pdf_document2}
 #'
 #' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
@@ -13,13 +15,14 @@
 #' library(bookdown)
 #' draft("MyArticle.Rmd", template = "lab_report", package = "rmdtemplates")
 #' }
-#'
 #' @export
 lab_report <- function(...,
+                       automark = TRUE,
                        number_sections = TRUE,
                        md_extensions = c("-autolink_bare_uris")){
   pdf_document_format(
     ...,
+    automark = automark,
     number_sections = number_sections,
     md_extensions = md_extensions,
     format = "lab_report",
